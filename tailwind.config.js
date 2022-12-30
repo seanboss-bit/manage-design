7782c9b3393a13d2aa2bf08675490dbe0da86f1d
Abubakar Musa <shaunaibek@gmail.com>
2022-12-30 13:14:33 +0100

    manage landing page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12,88%,69%)",
+        brightRedSuperLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        varyPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%,98%)",
+      },
+    },
+  },
+  plugins: [],
+};
